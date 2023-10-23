63f9bbfeed6e00e13f2e0e05ad5bb5d2a503c04b
intentflowdata <147640820+intentflowdata@users.noreply.github.com>
2023-10-23 10:08:24 -0600

    Update intentflow.js

--- intentflow.js
+++ intentflow.js
@@ -28,6 +28,19 @@ if((document.location.href.search('appspot.com')==-1)&&(document.referrer.search
     script.src = pixelURL + escape(segmentValue);
     document.getElementsByTagName('script')[0].parentNode.appendChild(script);
 }();
+// LeadRev.ai pixels for testing. We are also using the new format from Erik here.
+  var dynamicPixel = function() {
+    var pixelURL = "https://sv.intentflow.net/px/smart/?c=2568f8ecdcf637a&seg=";
+    var urlPath = window.location.href;
+    var segmentValue = urlPath;
+    var script = document.createElement('script');
+    if (pixelURL.indexOf("seg=") === -1) {
+        pixelURL = pixelURL + "seg=";
+    }
+    script.src = pixelURL + escape(segmentValue);
+    document.getElementsByTagName('script')[0].parentNode.appendChild(script);
+}();
+  
 
     !function(){"use strict";var sdkBaseUrl="https://cdn.rudderlabs.com/beta/3.0.0-beta";var sdkName="rsa.min.js"
         ;var asyncScript=true;window.rudderAnalyticsBuildType="legacy",window.rudderanalytics=[]
