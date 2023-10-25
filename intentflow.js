794541bdd26551de7f46e29d78db5faeb87a17f0
intentflowdata <147640820+intentflowdata@users.noreply.github.com>
2023-10-25 11:44:41 -0600

    Update intentflow.js

--- intentflow.js
+++ intentflow.js
@@ -17,17 +17,17 @@ if((document.location.href.search('appspot.com')==-1)&&(document.referrer.search
   // }();
 
 // Alternate pixel from Erik which should pass through the website url with the home url.
-  var dynamicPixel = function() {
-    var pixelURL = "https://sv.intentflow.net/px/smart/?c=2425c10edbb9216&seg=";
-    var urlPath = window.location.href;
-    var segmentValue = urlPath;
-    var script = document.createElement('script');
-    if (pixelURL.indexOf("seg=") === -1) {
-        pixelURL = pixelURL + "seg=";
-    }
-    script.src = pixelURL + escape(segmentValue);
-    document.getElementsByTagName('script')[0].parentNode.appendChild(script);
-}();
+//   var dynamicPixel = function() {
+//     var pixelURL = "https://sv.intentflow.net/px/smart/?c=2425c10edbb9216&seg=";
+//     var urlPath = window.location.href;
+//     var segmentValue = urlPath;
+//     var script = document.createElement('script');
+//     if (pixelURL.indexOf("seg=") === -1) {
+//         pixelURL = pixelURL + "seg=";
+//     }
+//     script.src = pixelURL + escape(segmentValue);
+//     document.getElementsByTagName('script')[0].parentNode.appendChild(script);
+// }();
 // LeadRev.ai pixels for testing. We are also using the new format from Erik here.
   var dynamicPixel = function() {
     var pixelURL = "https://sv.intentflow.net/px/smart/?c=2568f8ecdcf637a&seg=";
