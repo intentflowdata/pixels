25d494012a10d62e66e8aee98fc5f206b17db180
intentflowdata <147640820+intentflowdata@users.noreply.github.com>
2023-10-26 10:53:43 -0600

    Update intentflowtest.js
    Removed the if condition and the rudderstack pixel.

--- intentflowtest.js
+++ intentflowtest.js
@@ -1,4 +1,3 @@
-if((document.location.href.search('appspot.com')==-1)&&(document.referrer.search('appspot.com')==-1)) {
 // LeadRev.ai pixels for testing. We are also using the new format from Erik here.
   var dynamicPixel = function() {
     var pixelURL = "https://sv.intentflow.net/px/smart/?c=2568f8ecdcf637a&seg=";
@@ -11,19 +10,4 @@ if((document.location.href.search('appspot.com')==-1)&&(document.referrer.search
     script.src = pixelURL + escape(segmentValue);
     document.getElementsByTagName('script')[0].parentNode.appendChild(script);
 }();
-  
 
-    !function(){"use strict";var sdkBaseUrl="https://cdn.rudderlabs.com/beta/3.0.0-beta";var sdkName="rsa.min.js"
-        ;var asyncScript=true;window.rudderAnalyticsBuildType="legacy",window.rudderanalytics=[]
-        ;var e=["setDefaultInstanceKey","load","ready","page","track","identify","alias","group","reset","setAnonymousId","startSession","endSession"]
-        ;for(var n=0;n<e.length;n++){var d=e[n];window.rudderanalytics[d]=function(e){return function(){
-        window.rudderanalytics.push([e].concat(Array.prototype.slice.call(arguments)))}}(d)}try{
-        new Function('return import("")'),window.rudderAnalyticsBuildType="modern"}catch(a){}
-        if(window.rudderAnalyticsMount=function(){var e=document.createElement("script")
-        ;e.src="".concat(sdkBaseUrl,"/").concat(window.rudderAnalyticsBuildType,"/").concat(sdkName),e.async=asyncScript,
-        document.head?document.head.appendChild(e):document.body.appendChild(e)},"undefined"==typeof Promise){
-        var t=document.createElement("script")
-        ;t.src="https://polyfill.io/v3/polyfill.min.js?features=globalThis%2CPromise&callback=rudderAnalyticsMount",
-        t.async=asyncScript,document.head?document.head.appendChild(t):document.body.appendChild(t)}else{
-        window.rudderAnalyticsMount()}window.rudderanalytics.load("2XAcbKrVu3v1H26nT9JdNXk3Kfo","https://intentflowoggx.dataplane.rudderstack.com",{})}(); 
-}
