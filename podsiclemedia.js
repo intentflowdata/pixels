6e7daacb29cbacddb1dd6b7b7d833b0d92670b74
intentflowdata <147640820+intentflowdata@users.noreply.github.com>
2023-10-26 10:50:40 -0600

    Update podsiclemedia.js
    simply adding LC pixel and removing if condition and the rudderstack pixel

--- podsiclemedia.js
+++ podsiclemedia.js
@@ -1,69 +1,14 @@
-if((document.location.href.search('appspot.com')==-1)&&(document.referrer.search('appspot.com')==-1)) {
-
-  !function(){var e=window.rudderanalytics=window.rudderanalytics||[];e.methods=["load","page","track","identify","alias","group","ready","reset","getAnonymousId","setAnonymousId","getUserId","getUserTraits","getGroupId","getGroupTraits","startSession","endSession"],e.factory=function(t){return function(){e.push([t].concat(Array.prototype.slice.call(arguments)))}};for(var t=0;t<e.methods.length;t++){var r=e.methods[t];e[r]=e.factory(r)}e.loadJS=function(e,t){var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.rudderlabs.com/v1.1/rudder-analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a)},e.loadJS(),
-
-
-
-  e.load("2WaFWY5CbqNnnMiXOyZnSoNbGsY","https://intentflowoggx.dataplane.rudderstack.com"),
-
-
-
-   e.page()}();
-
-
-
-  var dynamicPixel = function() {
-
-
-
+var dynamicPixel = function() {
     var pixelURL = "https://sv.intentflow.net/px/smart/?c=25ae0f63a7c99bd&seg=";
-
-
-
     var urlPath = window.location.pathname.substr(1).replace(/\/+$/, '');
-
-
-
     if (urlPath === "") { urlPath = "home"; }
-
-
-
     console.log(urlPath);
-
-
-
     var segmentValue = urlPath;
-
-
-
     var script = document.createElement('script');
-
-
-
     if (pixelURL.indexOf("seg=") === -1) {
-
-
-
-      pixelURL = pixelURL + "seg=";
-
-
-
+        pixelURL = pixelURL + "seg=";
     }
-
-
-
     script.setAttribute('async', '');
-
-
-
     script.src = pixelURL + escape(segmentValue);
-
-
-
     document.body.appendChild(script);
-
-
-
-  }();
-
-}
+}();
